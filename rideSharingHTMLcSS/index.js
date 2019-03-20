f8a897a64cc760eb1e2b3e53183e3b8973b7ea2d
Thomas T <trampthomas16@gmail.com>
2019-03-20 13:45:43 -0500

    Added new HTML forms and new functionality

--- rideSharingHTMLcSS/index.js
+++ rideSharingHTMLcSS/index.js
@@ -1,19 +1,19 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyApmRAMvcs-SDBL08lykImZENr2rifpqlg",
     authDomain: "ride-sharing-4c8e7.firebaseapp.com",
     databaseURL: "https://ride-sharing-4c8e7.firebaseio.com",
     projectId: "ride-sharing-4c8e7",
     storageBucket: "ride-sharing-4c8e7.appspot.com",
     messagingSenderId: "217057301737"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 
 var db = firebase.firestore();
 
 //Listen for the form submit
-document.getElementById('postingForm').addEventListener('submit', submitPostingForm);
+document.getElementById('firstForm').addEventListener('submit', submitPostingForm);
 
 //submitting form
 function submitPostingForm(e) {
@@ -25,28 +25,28 @@ function submitPostingForm(e) {
         alert("First name must be entered");
         return false;
     }*/
-    
+
     var location = getInputValue('location');
-    
+
     var dateGoing = getInputValue('dateGoing');
-    
+
     var departureTime = getInputValue('departureTime');
-    
+
     var arrivalTime = getInputValue('arrivalTime');
-    
+
     var returnTime = getInputValue('returnTime');
-    
+
     var seats = getInputValue('seats');
-    
+
     var tripDescription = getInputValue('tripDescription');
-    
-    
+
+
 
     savePost(destination, location, dateGoing, departureTime, arrivalTime, returnTime, seats, tripDescription);
-    
-        
+
+
     //alert user registration was submitted
-   // document.querySelector('.postedAlert').style.display = 'block';
+    // document.querySelector('.postedAlert').style.display = 'block';
 
     //here i want to put code to direct to index as a logged in user
 
@@ -58,7 +58,7 @@ function getInputValue(id) {
 }
 
 // saving user info in database
-function savePost(destination, location, dateGoing, departureTime, arrivalTime,  returnTime, seats, tripDescription) {
+function savePost(destination, location, dateGoing, departureTime, arrivalTime, returnTime, seats, tripDescription) {
     db.collection("trips").add({
         destination: destination,
         location: location,
